a943bbfda60d98ba47d176d3a3be8cb0bc9fa06f
Andrew Rohne <andrew@siliconcreek.net>
2020-12-27 10:45:57 -0500

    test code update

--- gpio/test_readpulse.py
+++ gpio/test_readpulse.py
@@ -11,4 +11,9 @@ GPIO.setup(23, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
 def click23handler(channel):
     print("Click!")
 
-GPIO.add_event_detect(23, GPIO.RISING, callback = click23handler, bouncetime = 100)   
+GPIO.add_event_detect(23, GPIO.RISING, callback = click23handler, bouncetime = 100)
+
+try:
+    GPIO.wait_for_edge(24, GPIO.RISING)
+except KeyboardInterrupt:
+    GPIO.cleanup()
